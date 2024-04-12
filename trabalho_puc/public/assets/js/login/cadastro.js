d02bd8572b69b300d5848762b5b5c84b10a65150
Gustavo Vargas <126530464+gustavovargas7@users.noreply.github.com>
2024-04-12 20:13:48 -0300

    Adicionando arquivos
    Adicionando arquivos para começar a codar em equipe.

---
+++ trabalho_puc/public/assets/js/login/cadastro.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    const formCadUser = $('#cadas-user-data');
+    const btnEnvioForm = $('#enviar-form');
+    let usersData = [];
+    let dataUser = {};
+
+    $(formCadUser).on('submit', (e) => {
+        e.preventDefault();
+        getItens();
+    })
+
+    function getItens() {
+        dataUser = {
+            user: {
+                name: $('#name').val(),
+                sobrenome: $('#sobre-nome').val(),
+                cpf: $('#cpf').val(),
+                dataNascimento: $('#date-nasc').val(),
+            },
+            dataLoginUser: {
+                login: $('#login').val(),
+                senha: $('#senha').val(),
+                confirm: $('#confirm-senha').val(),
+            },
+            contatos: {
+                email: $('#email').val(),
+                numero: $('#numero').val(),
+            },
+            endereco: {
+                pais: $('#pais').val(),
+                estado: $('#estado').val(),
+                cidade: $('#cidade').val(),
+                bairro: $('#bairro').val(),
+                cep: $('#cep').val(),
+            },
+        };
+        usersData.push(dataUser);
+        localStorage.setItem('usersData', JSON.stringify(usersData));
+        console.log(usersData);
+    }
+});
