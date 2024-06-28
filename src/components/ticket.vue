e99557ee7af72efad0b50ed9ec3a718b6ba63ae6
PedroMartins73 <pedro276371@gmail.com>
2024-06-27 21:38:56 -0300

    Criação Tckets

---
+++ src/components/ticket.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="max-w-lg mx-left mb-3 bg-white shadow-md rounded-lg overflow-hidden">
+        <div class="flex">
+            <div class="w-1/3">
+                <img :src="dataTicket.imgUrl" class="object-cover w-full h-full" alt="...">
+            </div>
+            <div class="w-2/3 p-4">
+                <h5 class="text-xl font-semibold mb-5">INGRESSO</h5>
+                <p class="text-gray-700 mb-1">{{ dataTicket.event }}</p>
+                <p class="text-gray-500 mb-4">{{ dataTicket.local }}</p>
+                <p class="text-gray-300 text-sm mb-1">{{ dataTicket.date }} - {{ dataTicket.hour }}</p>
+                <p class="text-gray-300 text-sm">Qtde: {{ dataTicket.qtde }}</p>
+                <br>
+                <div class="">
+                    <p class="text-red-500 mb-2 border border-red-500 hover:bg-red-400 hover:text-white font-bold py-1 px-1 rounded">
+                        {{ dataTicket.situation }}
+                    </p>
+                    <p>Visualizar Ingresso</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Ticket',
+    props: {
+      dataTicket: {
+         type: Object,
+         required: true,
+      }
+    }
+}
+</script>
--- src/views/layout/tickets.vue
+++ src/views/layout/tickets.vue
@@ -1,5 +1,51 @@
 <template>
     <div>
-        <p>Meus ingressos:</p>
+        <Ticket v-for="ticket in tickets" :key="ticket.event" :dataTicket="ticket" />
     </div>
 </template>
+
+<script>
+
+import Ticket from "../../components/ticket.vue";
+import imgGaloxFla from '../img/galoxflamengo.webp';
+
+export default {
+    name: 'Tickets',
+    components: {
+        Ticket
+    },
+    data() {
+        return {
+            tickets: [
+                {
+                    imgUrl: imgGaloxFla,
+                    event: 'PAL',
+                    local: 'Local do Evento',
+                    date: 'Data do Evento',
+                    hour: 'Hora do Evento',
+                    qtde: 2,
+                    situation: 'Ativo'
+                },
+                {
+                    imgUrl: imgGaloxFla,
+                    event: 'FLA',
+                    local: 'Local do Evento',
+                    date: 'Data do Evento',
+                    hour: 'Hora do Evento',
+                    qtde: 2,
+                    situation: 'Ativo'
+                },
+                {
+                    imgUrl: imgGaloxFla,
+                    event: 'FLA',
+                    local: 'Local do Evento',
+                    date: 'Data do Evento',
+                    hour: 'Hora do Evento',
+                    qtde: 2,
+                    situation: 'Ativo'
+                },
+            ]
+        };
+    }
+}
+</script>
