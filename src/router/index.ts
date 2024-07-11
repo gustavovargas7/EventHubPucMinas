35ce5088a164c09d0e6dd7488184db73e80adcad
gustavovargas7 <gustavoherique1997@gmail.com>
2024-07-11 09:38:40 -0300

    router-principal

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/register',
       name: 'Register',
       component: Register,
     },
@@ -24,7 +24,7 @@ const router = createRouter({
       component: Login,
     },
     {
-      path: '/master',
+      path: '/',
       name: 'Master',
       component: Master,
       children: [
